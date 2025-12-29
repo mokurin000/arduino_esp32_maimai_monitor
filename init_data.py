4881f0c9bf975238a519e32ae991c5428ae79d46
莯凛 <mokurin000@gmail.com>
2025-12-29 15:43:42 +0800

    chore: reset count due to server maintaince

--- init_data.py
+++ init_data.py
@@ -3,9 +3,9 @@ from pathlib import Path
 file1 = Path("src") / "data" / "DX1.50"
 file2 = Path("data") / "DX1.50"
 
-succ = 64119
-timeout = 2119
-error = 634
+succ = 0
+timeout = 0
+error = 0
 
 for file in [file1, file2]:
     with open(file, "wb") as f:
